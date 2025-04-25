901675a98252435ba6233e543ff1cf9668449fc7
sanderPostma <spostma@sphereon.com>
2025-04-25 14:09:25 +0200

    chore: retry petuska publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import dev.petuska.npm.publish.extension.NpmPublishExtension
 
 allprojects {
     group = "com.sphereon.kmp"
-    version = "0.2.11"
+    version = "0.2.12"
 
     val npmVersion by extra { getNpmVersion() }
 }
